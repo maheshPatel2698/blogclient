712775af22e837081d7c06d039215d3efbef0b65
Mahesh Patel <mp8148515@gmail.com>
2023-01-11 11:53:22 +0530

    shadow edited

--- Components/Footer.jsx
+++ Components/Footer.jsx
@@ -5,7 +5,7 @@ const Footer = () => {
     return (
         <div className='w-full mt-2 font-bold border-purple-700 h-auto p-1 relative bottom-0 left-0 grid grid-cols-3 gap-2  max-md:grid-cols-2  '>
             {/* sitemap section */}
-            <div className='sitemap max-md:cols-span-1 flex h-40 shadow-lg justify-center items-start flex-col max-md:items-center  '>
+            <div className='sitemap max-md:cols-span-1 flex h-40 shadow-md justify-center items-start flex-col max-md:items-center  '>
                 <span className=' cursor-pointer hover:text-purple-700 text-lg  transition-all duration-300'>Sitemap</span>
                 <span className='  cursor-pointer hover:text-purple-700  transition-all duration-300'>About Us</span>
                 <span className=' cursor-pointer hover:text-purple-700  transition-all duration-300'>Contact Us</span>
@@ -14,7 +14,7 @@ const Footer = () => {
             </div>
 
             {/* socialmedia section */}
-            <div className='socialmedia max-md:col-span-1  flex h-40 shadow-lg justify-center items-start flex-col max-md:items-center '>
+            <div className='socialmedia max-md:col-span-1  flex h-40 shadow-md justify-center items-start flex-col max-md:items-center '>
                 <span className=' cursor-pointer hover:text-purple-700 text-lg transition-all duration-300'>Social</span>
                 <span className=' cursor-pointer hover:text-purple-700  transition-all duration-300 flex justify-center items-center'><AiOutlineLinkedin size={28} /> Linkedin</span>
                 <span className=' cursor-pointer hover:text-purple-700  transition-all duration-300 flex justify-center items-center'><AiOutlineFacebook size={28} />Facebook </span>
@@ -23,7 +23,7 @@ const Footer = () => {
             </div>
 
             {/* services */}
-            <div className='Services max-md:col-span-3 flex h-40 shadow-lg justify-center items-start flex-col max-md:items-center '>
+            <div className='Services max-md:col-span-3 flex h-40 shadow-md justify-center items-start flex-col max-md:items-center '>
                 <span className=' cursor-pointer hover:text-purple-700 text-lg transition-all duration-300'>Services</span>
                 <span className=' cursor-pointer hover:text-purple-700  transition-all duration-300 flex justify-center items-center'> Front End Development</span>
                 <span className=' cursor-pointer hover:text-purple-700  transition-all duration-300 flex justify-center items-center'>Back End Development </span>
