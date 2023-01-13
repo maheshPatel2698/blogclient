7f1cd5d053297ff67e573d446a0bebb244b4961c
Mahesh Patel <mp8148515@gmail.com>
2023-01-13 12:19:18 +0530

    blog page complete

--- Components/Navbar.jsx
+++ Components/Navbar.jsx
@@ -23,7 +23,7 @@ const Navbar = () => {
     }
 
     return (
-        <div className='sticky top-0 left-0 w-full h-14 z-50 flex justify-between items-center gap-3  max-md:shadow-md max-md:shadow-purple-600 rounded-full max-md:w-3/4  m-auto max-md:flex-col max-md:h-auto'>
+        <div className='sticky top-0 left-0 w-full h-14 z-50 flex justify-between items-center gap-3 max-md:bg-white max-md:shadow-md max-md:shadow-purple-600 rounded-full max-md:w-3/4  m-auto max-md:flex-col max-md:h-auto'>
             <div className="left  shadow-md rounded-full h-full flex justify-around items-center  w-6/12 max-md:shadow-none">
                 <span onClick={tggleSidebar} className=' p-1 cursor-pointer font-bold text-purple-700'>
                     <Link href='/'>BlogoForCoder</Link>
