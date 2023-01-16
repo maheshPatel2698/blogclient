64fa30fe990b4bc0e2287729a13f03eb9cd2465d
Mahesh Patel <mp8148515@gmail.com>
2023-01-16 20:33:15 +0530

    minor changes

--- Components/Navbar.jsx
+++ Components/Navbar.jsx
@@ -74,10 +74,10 @@ const Navbar = () => {
                 <span onClick={tggleSidebar} className='m-2 cursor-pointer hover:text-purple-700 transition-all duration-300'>
                     <Link href='/contact'>Contact</Link>
                 </span>
-                <span onClick={tggleSidebar} className='cursor-pointer hover:text-purple-700 transition-all duration-300 m-2'>
+                <span onClick={tggleSidebar} className=' m-2 cursor-pointer hover:text-purple-700 transition-all duration-300 m-2'>
                     <Link href='/services'>Services</Link>
                 </span>
-                <span onClick={tggleSidebar} className='cursor-pointer hover:text-purple-700 transition-all duration-300'>
+                <span onClick={tggleSidebar} className=' m-2 cursor-pointer hover:text-purple-700 transition-all duration-300'>
                     <Link href="/signup"> Sign Up </Link>
                 </span>
                 <span onClick={tggleSidebar} className='  m-2 cursor-pointer hover:text-purple-700 transition-all duration-300'>
